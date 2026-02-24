2c2515d0ba2d6dd34a91358ce323c308c4104f8f
shrishaanth <shrishaanth2024@gmail.com>
2026-02-24 19:26:28 +0530

    Intial Setup

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
+
