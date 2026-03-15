370740d600e800b09edc0491527f5e047d2ade17
PogrammerLord <notrued@gmail.com>
2026-03-15 18:30:07 +0530

    Pages Commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./Pages/Index";
+import MyTasksPage from "./Pages/MyTasksPage";
+import NotFound from "./Pages/NotFound";
 
 const App = () => {
   return (
-    <div>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/my-tasks" element={<MyTasksPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
